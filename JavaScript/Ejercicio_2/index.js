ed29c2e0b2a5ec3a816e42ff962573efdbecd50b
Johan <101450667+JohanRF@users.noreply.github.com>
2022-12-13 22:38:20 -0500

    Add files via upload

---
+++ JavaScript/Ejercicio_2/index.js
@@ -0,0 +1,16 @@
+
+const lista ={ 
+
+	nombre : "Johan Rivera",
+	edad : 21 ,
+	"¿Eres desarrollador?" : "true",
+	fecha : new Date(2001, 7, 13),
+	libro : {
+		titulo: "Francotirador",
+		autor: "Chris Kyle, Scoot McEwen, Jim DeFelice",
+		fecha: new Date(2012, 0, 2),
+		url: "https://www.planetadelibros.com/libro-el-francotirador/174642"
+	}
+}
+
+console.log(lista);
